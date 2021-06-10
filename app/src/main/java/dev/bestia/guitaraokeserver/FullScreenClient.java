ec3634114943915a34dc38123e1bdd72e9bcf2b3
Luciano <luciano.bestia@gmail.com>
2021-06-10 15:17:22 +0200

    Leader page in webview, server text in dialog

---
+++ app/src/main/java/dev/bestia/guitaraokeserver/FullScreenClient.java
@@ -0,0 +1,41 @@
+package dev.bestia.guitaraokeserver;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.widget.RelativeLayout;
+
+public class FullScreenClient extends WebChromeClient {
+
+    RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT);
+
+    ViewGroup parent;
+    ViewGroup content;
+    View customView;
+
+    public FullScreenClient(ViewGroup parent, ViewGroup content){
+        this.parent = parent;
+        this.content = content;
+    }
+
+    @Override
+    public void onShowCustomView(View view, CustomViewCallback callback) {
+        super.onShowCustomView(view, callback);
+
+        customView = view;
+        view.setLayoutParams(layoutParams);
+        parent.addView(view);
+        content.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onHideCustomView() {
+        super.onHideCustomView();
+
+        content.setVisibility(View.VISIBLE);
+        parent.removeView(customView);
+        customView = null;
+    }
+}
